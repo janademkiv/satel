7e897c56a974ae29888d7650a1fe8e16826e0c9a
Яна ДемкивÐ <studioarttt@gmail.com>
2020-06-16 23:43:56 +0400

    init

---
+++ src/main/java/com/jdemkiv/satel/SatelApplication.java
@@ -0,0 +1,13 @@
+package com.jdemkiv.satel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SatelApplication.class, args);
+    }
+
+}
